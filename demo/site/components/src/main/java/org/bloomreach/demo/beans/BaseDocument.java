a40ffab74bbb079d32168fb1cd83fc6357daa7fc
ksalic <kenan.salic@bloomreach.com>
2020-03-19 17:19:40 +0100

    demo

---
+++ demo/site/components/src/main/java/org/bloomreach/demo/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.bloomreach.demo.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="demo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
